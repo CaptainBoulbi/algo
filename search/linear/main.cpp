7c91b03a50e1aa9c707367a4259c00f9fd63b13b
_N3m0 <n3m0o7@hotmail.com>
2023-07-08 12:24:44 +0200

    linear search

---
+++ search/linear/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int linearSearch(int arr[], int len, int v){
+	for (int i=0; i<len; i++){
+		if (arr[i] == v){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main(){
+	int arr[] = {9,5,7,3,8,0,1,5,2,4,6};
+	int len = sizeof(arr)/sizeof(arr[0]);
+
+	std::cout << arr[0];
+	for (int i=1; i<len; i++){ std::cout << "," << arr[i]; }
+	std::cout << std::endl;
+
+	std::cout << "index of 8 : " << linearSearch(arr, len, 8) << std::endl;
+	std::cout << "index of 5 : " << linearSearch(arr, len, 5) << std::endl;
+
+	return 0;
+}
