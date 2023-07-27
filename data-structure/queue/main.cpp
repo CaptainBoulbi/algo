da6d9dff981dd04d0ef9f4a22431ac6cecb42b1b
_N3m0 <n3m0o7@hotmail.com>
2023-07-27 10:23:04 +0200

    queue finished (?)

--- data-structure/queue/main.cpp
+++ data-structure/queue/main.cpp
@@ -10,18 +10,38 @@ template <class T>
 class Queue{
 	public:
 		Queue(){
-			//this->head = new node<T>;
-			//this->tail = this->head;
+			this->head = NULL;
+			this->tail = this->head;
 		};
 
 		T enqueue(T value){
-			
+			if (this->tail == NULL){
+				this->tail = new node<T>;
+				this->tail->value = value;
+				this->head = this->tail;
+			}else{
+				node<T>* newNode = new node<T>;
+				newNode->value = value;
+				this->tail->next = newNode;
+				this->tail = newNode;
+			}
+			return value;
 		};
 
-		T deque(){};
+		T deque(){
+			if (this->head != NULL){
+				T returnValue = this->head->value;
+				node<T>* nodeToRemove = this->head;
+				this->head = this->head->next;
+				delete nodeToRemove;
+				return returnValue;
+			}
+			return (int)NULL;
+		};
 
-		T peek(){};
-
+		T peek(){
+			return this->head == NULL ? (int)NULL : this->head->value;
+		};
 	private:
 		node<T>* head;
 		node<T>* tail;
@@ -33,6 +53,8 @@ int main(){
 	Q.enqueue(5);
 	std::cout << Q.peek() << std::endl;
 	Q.enqueue(9);
+	std::cout << Q.peek() << std::endl;
+	Q.deque();
 	std::cout << Q.peek() << std::endl;
 	Q.deque();
 	std::cout << Q.peek() << std::endl;
