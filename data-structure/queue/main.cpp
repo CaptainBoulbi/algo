807b4d020dc486d2723802b58a0f2c3ccf4e81cd
_N3m0 <n3m0o7@hotmail.com>
2023-07-27 10:47:10 +0200

    queue : add length

--- data-structure/queue/main.cpp
+++ data-structure/queue/main.cpp
@@ -11,9 +11,11 @@ class Queue{
 	public:
 		Queue(){
 			this->head = this->tail = NULL;
+			this->len = 0;
 		};
 
 		T enqueue(T value){
+			this->len++;
 			node<T>* newNode = new node<T> {value, NULL};
 			if (!this->tail)
 				this->head = this->tail = newNode;
@@ -24,6 +26,7 @@ class Queue{
 
 		T deque(){
 			if (!this->head) return (int)NULL;
+			this->len--;
 			T returnValue = this->head->value;
 			node<T>* nodeToRemove = this->head;
 			this->head = this->head->next;
@@ -34,22 +37,27 @@ class Queue{
 		T peek(){
 			return this->head == NULL ? (int)NULL : this->head->value;
 		};
+
+		int length(){
+			return this->len;
+		};
 	private:
 		node<T>* head;
 		node<T>* tail;
+		int len;
 };
 
 int main(){
 
 	Queue<int> Q;
 	Q.enqueue(5);
-	std::cout << Q.peek() << std::endl;
+	std::cout << Q.length() << " : " << Q.peek() << std::endl;
 	Q.enqueue(9);
-	std::cout << Q.peek() << std::endl;
+	std::cout << Q.length() << " : " << Q.peek() << std::endl;
 	Q.deque();
-	std::cout << Q.peek() << std::endl;
+	std::cout << Q.length() << " : " << Q.peek() << std::endl;
 	Q.deque();
-	std::cout << Q.peek() << std::endl;
+	std::cout << Q.length() << " : " << Q.peek() << std::endl;
 
 	return 0;
 }
