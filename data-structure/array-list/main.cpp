4dce9d6c9a5ad5de4e56034100958663e9f2e441
_N3m0 <n3m0o7@hotmail.com>
2023-07-28 09:17:05 +0200

    add array list

--- data-structure/array-list/main.cpp
+++ data-structure/array-list/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 // https://en.cppreference.com/w/cpp/container/list
 // main array in public (access l.array[5] = 9;)
@@ -6,19 +7,53 @@
 template <class T>
 class ArrayList{
 	public:
-		ArrayList(){};
+		T* array;
 
-		std::string toString(){};
+		ArrayList(int capacity){
+			this->array = new T[capacity];
+			this->cap = capacity;
+			this->len = 0;
+		};
 
-		int length(){};
+		std::string toString(){
+			std::string string;
+			for (int i=0; i<this->len; i++){
+				string += std::to_string(this->array[i]) + ' ';
+			}
+			return string;
+		};
 
-		int capacity(){};
+		int length(){
+			return this->len;
+		};
 
-		int resize(){};
+		int capacity(){
+			return this->cap;
+		};
 
-		T set(){};
+		int resize(int newCapacity){
+			this->cap = newCapacity;
+			T* newArr = new T[newCapacity];
+			for (int i=0; i<newCapacity; i++){
+				newArr[i] = this->array[i];
+			}
+			delete this->array;
+			this->array = newArr;
+			return newCapacity;
+		};
 
-		T get(){};
+		T set(int index, T value){
+			if (index >= this->len) this->len = index+1;
+			if (index >= this->cap) this->resize(this->cap + std::sqrt(this->cap));
+			this->array[index] = value;
+			return value;
+		};
+
+		T get(int index){
+			// sould throw an error
+			if (index >= this->len) return (T)NULL;
+			return this->array[index];
+		};
 
 	private:
 		int len;
@@ -26,6 +61,43 @@ class ArrayList{
 };
 
 int main(){
+	ArrayList<int> l(5);
+	std::cout << "len : " << l.length() << " cap : " << l.capacity() << std::endl;
+	std::cout << "disp : " << l.toString() << std::endl;
+
+	int y = l.capacity()+5;
+	for (int i=0; i<y; i++){
+		l.set(i,i*i);
+		std::cout << "add : " << i*i << std::endl;
+	}
+
+	std::cout << "len : " << l.length() << " cap : " << l.capacity() << std::endl;
+	std::cout << "disp : " << l.toString() << std::endl;
+
+	// sould throw an error
+	std::cout << "get : " << l.get(l.length()+5) << " at : " << l.length()+5 << std::endl;
+	std::cout << "get : " << l.get(-1) << " at : -1" << std::endl;
+
+	std::cout << "man get : " << l.array[5] << " at 5" << std::endl;
+	std::cout << "man get : " << l.array[-1] << " at -1" << std::endl;
+
+	std::cout << "char array list" << std::endl;
+
+	ArrayList<char> c(5);
+
+	for (int i=0; i<10; i+=2){
+		c.set(i,'c');
+		c.set(i+1,'a');
+	}
+
+	for (int i=0; i<c.length(); i++){
+		std::cout << c.get(i) << ' ';
+	}
+	std::cout << std::endl;
+
+	// ;{
+	std::cout << c.toString() << std::endl;
+	std::cout << c.length() << ' ' << c.capacity() << std::endl;
 
 	return 0;
 }
