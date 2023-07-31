890916bf2c415bb3840d45cb681abaa5428bdc82
_N3m0 <n3m0o7@hotmail.com>
2023-07-31 11:01:54 +0200

    make bfs really search

--- data-structure/tree/breadth-first-search/main.cpp
+++ data-structure/tree/breadth-first-search/main.cpp
@@ -50,92 +50,6 @@ class Queue{
 		int len;
 };
 
-template <class T>
-class ArrayList{
-	public:
-		T* array;
-		ArrayList(int capacity);
-		~ArrayList();
-		std::string toString();
-		int length();
-		int capacity();
-		int resize(int newCapacity);
-		T set(int index, T value);
-		T get(int index);
-		T pop();
-
-	private:
-		int len;
-		int cap;
-};
-
-template <class T>
-ArrayList<T>::ArrayList(int capacity){
-	this->array = new T[capacity];
-	this->cap = capacity;
-	this->len = 0;
-};
-
-template <class T>
-ArrayList<T>::~ArrayList(){
-	delete this->array;
-};
-
-template <class T>
-std::string ArrayList<T>::toString(){
-	std::string string;
-	for (int i=0; i<this->len; i++){
-		string += std::to_string(this->array[i]) + ' ';
-	}
-	return string;
-};
-
-template <class T>
-int ArrayList<T>::length(){
-	return this->len;
-};
-
-template <class T>
-int ArrayList<T>::capacity(){
-	return this->cap;
-};
-
-template <class T>
-int ArrayList<T>::resize(int newCapacity){
-	this->cap = newCapacity;
-	T* newArr = new T[newCapacity];
-	for (int i=0; i<newCapacity || i<this->len; i++){
-		newArr[i] = this->array[i];
-	}
-	delete this->array;
-	this->array = newArr;
-	return newCapacity;
-};
-
-template <class T>
-T ArrayList<T>::set(int index, T value){
-	if (index >= this->len) this->len = index+1;
-	if (index >= this->cap) this->resize(index + std::sqrt(index));
-	this->array[index] = value;
-	return value;
-};
-
-template <class T>
-T ArrayList<T>::get(int index){
-	if (index >= this->len) throw "out bound array";
-	return this->array[index];
-};
-
-template <class T>
-T ArrayList<T>::pop(){
-	if (this->len <= 0) throw "pop empty list";
-	T value = this->array[this->len-1];
-	int gap = this->cap - std::sqrt(this->cap);
-	this->len--;
-	if (this->len <= gap) this->resize(gap+1);
-	return value;
-};
-
 template <typename T>
 struct binaryNode{
 	T value;
@@ -144,19 +58,21 @@ struct binaryNode{
 };
 
 template <typename T>
-ArrayList<T>* bfs(binaryNode<T>* curr){
-	ArrayList<T>* arr = new ArrayList<T>(10);
+bool bfs(binaryNode<T>* curr, T needle, binaryNode<T>** ptr){
 	Queue<binaryNode<T>*> q;
-	
 	q.enqueue(curr);
 
 	while (q.length()){
+		if (curr->value == needle){
+			*ptr = curr;
+			return true;	
+		}
 		if (curr->left) q.enqueue(curr->left);
 		if (curr->right) q.enqueue(curr->right);
-		arr->set(arr->length(), q.deque()->value);
+		q.deque();
 		curr = q.peek();
 	}
-	return arr;
+	return false;
 }
 
 int main(){
@@ -170,10 +86,10 @@ int main(){
 	binaryNode<int> chr = {6, &chrl, &chrr};
 
 	binaryNode<int> head = {5, &chl, &chr};
+	binaryNode<int>* res = NULL;
 
-	ArrayList<int>* res = bfs(&head);
-	std::cout << res->toString() << std::endl;
-	delete res;
+	if (!bfs(&head, 9, &res)) std::cout << "not found" << std::endl;
+	else std::cout << "value 9 at " << res << " * : " << res->value << std::endl;
 
 	return 0;
 }
